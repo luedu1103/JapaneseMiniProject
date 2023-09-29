7aec0e3acc5d3db9b0a0e002a73ff64fce4cda7a
luedu1103 <luis.calle3@unmsm.edu.pe>
2023-09-28 23:16:58 -0500

    V2: Much better

--- Main.js
+++ Main.js
@@ -1,37 +1,6 @@
+// Lista de nombres para autocompletar
+var names = ['Neo', 'Ibiyemi', 'Olayinka', 'Jonathan', 'Stephen', 'Fisayo', 'Gideon', 'Mezie', 'Oreoluwa', 'Jordan', 'Enkay', 'Michelle', 'Jessica'];
 
-var names = ["Alice", "Bob", "Charlie", "Dave", "Eve"];
-  var maxResults = 5; // Limit the number of displayed results
-
-  function autocomplete() {
-    var input = document.getElementById('autocomplete-input').value;
-    var autocompleteResults = document.getElementById('autocomplete-results');
-    autocompleteResults.innerHTML = '';
-
-    if (input.trim() === '') {
-      return; // No input, so don't display results
-    }
-
-    var count = 0; // To limit the number of displayed results
-
-    for (var i = 0; i < names.length; i++) {
-      if (names[i].toLowerCase().startsWith(input.toLowerCase())) {
-        var node = document.createElement('div');
-        node.textContent = names[i];
-        node.onclick = function() {
-          document.getElementById('autocomplete-input').value = this.textContent;
-          clearResults();
-        };
-        autocompleteResults.appendChild(node);
-
-        count++;
-        if (count >= maxResults) {
-          break; // Reached the maximum number of results
-        }
-      }
-    }
-  }
-
-  function clearResults() {
-    var autocompleteResults = document.getElementById('autocomplete-results');
-    autocompleteResults.innerHTML = '';
-  }
+// Obtén la entrada y la lista desplegable
+var input = document.getElementById('name_input');
+var list = document.getElementById('names_list')
